4d0e02293d24739502aa3151d0f37e928ebbea1a
Anjana2005 <anjanakattungal@gmail.com>
2026-07-17 12:17:08 +0530

    Create floradmin superuser during Render build

--- build.sh
+++ build.sh
@@ -7,3 +7,19 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Create / update superuser (free-tier safe; no one-off jobs needed)
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+U = get_user_model()
+u, created = U.objects.get_or_create(
+    username='floradmin',
+    defaults={'email': 'floradmin@flora.com'},
+)
+u.set_password('flora@123')
+u.is_staff = True
+u.is_superuser = True
+u.is_active = True
+u.save()
+print('SUPERUSER_OK created=' + str(created))
+"
